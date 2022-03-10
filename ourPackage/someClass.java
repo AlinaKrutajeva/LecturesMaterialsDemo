828e30fafb605e40a0701ba1adb5188a9c5d285b
AlinaKrutajeva <a_k_a@inbox.lv>
2022-03-10 19:11:47 +0200

    - our new changes - ...

---
+++ ourPackage/someClass.java
@@ -0,0 +1,8 @@
+package ourPackage;
+
+public class someClass {
+    public static void main(String[] args) {
+        String name = "name";
+        System.out.println("Hello World!" + name);
+    }
+}
