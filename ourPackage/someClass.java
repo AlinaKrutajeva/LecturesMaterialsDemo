9b01ac1a4e166fe30ffcd54cb15b23041b0c09c6
AlinaKrutajeva <a_k_a@inbox.lv>
2022-03-10 20:12:18 +0200

    all files from lectures

--- ourPackage/someClass.java
+++
@@ -1,8 +0,0 @@
-package ourPackage;
-
-public class someClass {
-    public static void main(String[] args) {
-        String name = "name";
-        System.out.println("Hello World!" + name);
-    }
-}
